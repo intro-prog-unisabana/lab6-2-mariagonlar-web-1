86214cccd6a1fa23ca442899bfca15260f3d07e2
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-07 10:36:41 -0500

    store 1

--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,5 @@
 # Write your code here!
+def temp_and_color(diccionario):
+    temp_value = diccionario.get("temp", None)
+    color_value = diccionario.get("color", None)
+    return temp_value, color_value
