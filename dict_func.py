cde74ef89d9f7a02052237a66d3dc6ddc2b0f1d6
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-07 10:52:34 -0500

    func 1

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,16 @@
 # Write your code here!
+def employee_print(employee_info):
+    name = employee_info.pop("Name", "N/A")
+    salary = employee_info.pop("Salary", "N/A")
+    role = employee_info.pop("Role", "N/A")
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+    if employee_info:
+        for key, value in employee_info.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
+    employee_info["Name"] = name
+    employee_info["Salary"] = salary
+    employee_info["Role"] = role
