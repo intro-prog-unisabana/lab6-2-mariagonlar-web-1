d74fdab4fbe1886a33f3dd247c0b19b9752ae82d
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-07 16:59:58 -0500

    estudiantes 2

--- grades_manager.py
+++ grades_manager.py
@@ -2,5 +2,27 @@ def initialize_dict(student_name, subject_grades):
     student_dict = {
         student_name: subject_grades
     }
-
     return student_dict
+def add_student(student_grades=None):
+    if student_grades is None:
+        student_grades = {}
+    student_name = input("Enter student name:\n").title()
+
+    subjects = {}
+
+    while True:
+        entrada = input(
+            "Enter subject and grade (or 'exit' to finish):\n"
+        )
+        if entrada.lower() == "exit":
+            break
+        subject, grade = entrada.split(",")
+        subject = subject.title()
+        grade = float(grade)
+        subjects[subject] = grade
+    student_grades[student_name] = subjects
+    print(
+        f"Student {student_name} successfully added to the grades management system."
+    )
+    return student_grades
+
